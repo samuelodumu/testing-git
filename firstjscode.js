034a828ec8c1590df96c38dd4a7180d4c93e9117
samuel <themainsamuel@gmail.com>
2022-11-14 21:59:36 +0100

    Made html responsive, Updated python and JS files

--- firstjscode.js
+++ firstjscode.js
@@ -1,1 +1,8 @@
-console.log("This is absurd")
+age = Math.floor(Math.random() * 10) + 15;
+console.log("You are " + age + " years old");
+
+if (age <= 18) {
+    console.warn("You're not old enough to enter.")
+} else {
+    console.log("Come in Sir/Ma'am")
+}
--- firstpythonscript.py
+++ firstpythonscript.py
@@ -1,7 +1,11 @@
-things {
-    {water : drop},
-    {sand: grain},
-    {nation : person},
-    {apocalypse : accident},
+animal_group_terms = {
+    "lion" : "A pride of Lions",
+    "tiger" : "An ambushment of tigers",
+    "owl" : "A parliament of owls",
+    "raven" : "An unkindness of ravens",
+    "giraffe" : "A tower of giraffes"
 }
-print(things[1])
+print(animal_group_terms["lion"])
+if "raven" in animal_group_terms:
+    print("Yes a group of ravens is an unkindness :)")
+    
